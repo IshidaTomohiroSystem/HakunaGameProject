6113044184164fb67fd3e84839da5915099a086f
IshidaTomohiroSystem <totoz_ishida@au.com>
2023-03-07 21:04:02 +0900

    update effect

---
+++ Assets/Scripts/Character/EffectPlay.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.VFX;
+
+public class EffectPlay : MonoBehaviour
+{
+    [SerializeField] VisualEffect effect;
+
+    private void Awake()
+    {
+        effect.SendEvent("StopPlay");
+        effect.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            effect.SendEvent("OnPlay");
+            //OnPlayはEvent Nameで指定した任意の名称
+        }
+
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    effect.SetFloat("Direction", -1);
+        //    effect.gameObject.transform.localPosition = new Vector3(
+        //        effect.gameObject.transform.localPosition.x,
+        //        effect.gameObject.transform.localPosition.y,
+        //        effect.gameObject.transform.localPosition.z);
+        //}
+        //
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    effect.SetFloat("Direction", 1);
+        //    effect.gameObject.transform.localPosition = new Vector3(
+        //       -effect.gameObject.transform.localPosition.x,
+        //       effect.gameObject.transform.localPosition.y,
+        //       effect.gameObject.transform.localPosition.z);
+        //}
+    }
+}
---
+++ Assets/Scripts/Character/LazerDirection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class LazerDirection : MonoBehaviour
+{
+    [SerializeField] VisualEffect effect;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            effect.SetFloat("Direction", -1);
+        }
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+            effect.SetFloat("Direction", 1);
+        }
+    }
+}
